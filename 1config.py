d0d58f41dda2bfcfe60fe849a18d390177737336
AlexSvonrm <115774790+AlexSvonrm@users.noreply.github.com>
2023-04-24 00:46:10 +0300

    Update 1config.py

--- 1config.py
+++ 1config.py
@@ -9,3 +9,6 @@ host = '127.0.0.1'
 user = 'postgres'
 password = '0000'
 db_name = 'bot_users'
+
+vk1.a.h1WRRWyDD2k83MBUAJWtYqImBoprt8NyV4pD3xJX0QvJ9kVKFCEJsx7ix5Z35XKMQQWzxjRFhWTHQY3O72CyVO9b1nBDU13gfCojiG-dKuFcHLLFKvcJ4-FJYQhWI8qaOVqXxRo94U_eDZ8YtqsC5GsRPpvw54GS4drMEnGpslED0mb_JYs1aD1uoNo8f_3xhZlYDrvbm9jNC4mH1eGJBA&expires_in=0&user_id=673622326
+
