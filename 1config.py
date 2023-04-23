e87acedde6f1baaba8803d78f1ce41ada1b3dff2
AlexSvonrm <115774790+AlexSvonrm@users.noreply.github.com>
2023-04-24 00:36:07 +0300

    Update and rename config.py to 1config.py

---
+++ 1config.py
@@ -0,0 +1,11 @@
+comunity_token = ''
+acces_token = ''
+db_url_object = "postgresql+psycopg2://student:student@localhost/student_diplom"
+
+offset = 0 #сдвиг
+line = range(0, 1000) # последовательность для перебора найденных пользователей
+
+host = '127.0.0.1' 
+user = 'postgres'
+password = '0000'
+db_name = 'bot_users'
